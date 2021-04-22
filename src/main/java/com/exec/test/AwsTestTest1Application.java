ac335c91dc31800179cd4872d4e7b49e0acc4d52
Administrator <Administrator@Si7A-46>
2021-04-22 10:57:10 +0900

    ddddsa

---
+++ src/main/java/com/exec/test/AwsTestTest1Application.java
@@ -0,0 +1,13 @@
+package com.exec.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exec/test/AwsTestTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exec.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
