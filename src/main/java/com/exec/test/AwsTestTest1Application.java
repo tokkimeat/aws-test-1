4b4e40b98f6f3bcd72686b83ddc378e1c9c0b063
Administrator <Administrator@Si7A-46>
2021-04-22 11:18:12 +0900

    ddddd

--- src/main/java/com/exec/test/AwsTestTest1Application.java
+++ src/main/java/com/exec/test/AwsTestTest1Application.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsTestTest1Application {
 
-	public static void main(String[] args) {   
-		SpringApplication.run(AwsTestTest1Application.class, args);   
-		
-		
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestTest1Application.class, args);
+
 	}
 
 }
