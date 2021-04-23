c4f940be7741d00ae762533a7ab71dcaecdd4669
Administrator <Administrator@Si7A-46>
2021-04-23 10:21:26 +0900

    http => https

--- src/main/java/com/exec/test/AwsTestTest1Application.java
+++ src/main/java/com/exec/test/AwsTestTest1Application.java
@@ -1,7 +1,14 @@
 package com.exec.test;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AwsTestTest1Application {
@@ -9,6 +16,29 @@ public class AwsTestTest1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(AwsTestTest1Application.class, args);
 
+	}
+	
+	@Bean
+	public ServletWebServerFactory swsFactoryInit() {
+		TomcatServletWebServerFactory swsFactory =new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setPort(80);
+		connector.setRedirectPort(443);
+		swsFactory.addAdditionalTomcatConnectors(connector);
+		return swsFactory;
 	}
 
 }
